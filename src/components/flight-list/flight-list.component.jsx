1aba84e172e92d3b516e751f8664d3661d178471
Jan Krupa <jan.krupa@vsb.cz>
2020-05-21 13:34:27 +0200

    Add header info for flight data

--- src/components/flight-list/flight-list.component.jsx
+++ src/components/flight-list/flight-list.component.jsx
@@ -3,14 +3,23 @@ import './flight-list.style.css'
 import { Flight } from '../flight/flight.component'
 
 export const FlightList = (props) => {
-    console.log(props)
+  console.log(props)
 
-      return(
-          <div className='flight-list'>
-            {props.flights.map(flight => (
-              <Flight key={flight.id} flight={flight} type={props.type}/>
-            ))}
-          </div>
-    )
+  return (
+    <div className='flight-list'>
+      <div className="header">
+        <p> ICAO </p>
+        <p> firstSeen </p>
+        <p> estDepartureAirport </p>
+        <p> lastSeen </p>
+        <p> estArrivalAirport </p>
+        <p> callsign </p>
+      </div>
+
+      {props.flights.map(flight => (
+        <Flight key={flight.id} flight={flight} type={props.type} />
+      ))}
+    </div>
+  )
 }
 
